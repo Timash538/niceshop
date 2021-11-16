8bcf7d846af199f3823ce06e039ea07f3ff651e2
Timash538 <timash538@gmail.com>
2021-11-16 23:56:39 +0300

    Initial commit

---
+++ src/main/java/com/niceshop/niceshop/NiceshopApplication.java
@@ -0,0 +1,13 @@
+package com.niceshop.niceshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiceshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiceshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niceshop/niceshop/NiceshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niceshop.niceshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiceshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
